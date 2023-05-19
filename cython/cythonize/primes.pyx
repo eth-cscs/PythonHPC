a96c97359f5a961140c29e03e034b4de6b24a0dc
Theofilos Manitaras <manitaras@cscs.ch>
2023-05-19 15:24:27 +0200

    Use C math functions
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

--- cython/cythonize/primes.pyx
+++ cython/cythonize/primes.pyx
@@ -1,13 +1,21 @@
-from math import sqrt, ceil
+from libc.math cimport sqrt, ceil
 
+from cython cimport boundscheck, wraparound, cdivision
+
+
+@boundscheck(False)
+@wraparound(False)
+@cdivision(True)
 cpdef bint is_prime(int n):
-    cdef int i, n_sqrt = ceil(sqrt(n))
+    cdef int i
+
     if n == 1 or n == 3:
         return True
     elif n == 2:
         return False
-    else :
-        for i in range(2, n_sqrt):
+    else:
+        for i in range(2, <int>ceil(sqrt(n))):
             if n % i == 0:
                 return False
+
     return True
