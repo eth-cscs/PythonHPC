3e4a0ef974ab9d16cdb88215780cd51beda5e626
Theofilos Manitaras <manitaras@cscs.ch>
2023-06-20 16:56:15 +0200

    Add another OpenMP function
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

--- cython/openmp/hello_omp.pyx
+++ cython/openmp/hello_omp.pyx
@@ -2,12 +2,15 @@ from cython cimport nogil
 from cython.parallel cimport parallel
 from libc.stdio cimport printf
 
-from openmp cimport omp_get_num_threads, omp_get_thread_num
+from openmp cimport (omp_get_num_threads, omp_get_thread_num,
+                     omp_get_num_procs)
 
 
 def hello_omp():
-    printf("---Hello from serial region---\n")
+    print("Inside serial region")
+    print("Number of Processors:", omp_get_num_procs())
+    print()
 
     with nogil, parallel():
-        printf("Hello from %2d/%2d \n", omp_get_thread_num(),
+        printf("Hello from thread %d out of %d\n", omp_get_thread_num(),
                omp_get_num_threads())
